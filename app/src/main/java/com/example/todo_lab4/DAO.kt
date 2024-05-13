044f5e78f8bd5256ae188bcafee053daa6f1c477
Tharusha Rashmika <rashmikatharusha8@gmail.com>
2024-05-13 11:56:48 +0530

    some updates

--- app/src/main/java/com/example/todo_lab4/DAO.kt
+++ app/src/main/java/com/example/todo_lab4/DAO.kt
@@ -2,27 +2,26 @@ package com.example.todo_lab4
 
 import androidx.room.*
 
+// Data Access Object (DAO) interface for Room database operations
 @Dao
 interface DAO {
+    // Insert a new task into the database
     @Insert
     suspend fun insertTask(entity: Entity)
 
+    // Update an existing task in the database
     @Update
     suspend fun updateTask(entity: Entity)
 
-
+    // Delete a task from the database
     @Delete
     suspend fun deleteTask(task: Entity)
-    @Query("Delete from to_do")
+
+    // Delete all tasks from the database
+    @Query("DELETE FROM to_do")
     suspend fun deleteAll()
 
-    @Query("Select * from to_do")
-    suspend fun getTasks():List<CardInfo>
+    // Retrieve all tasks from the database
+    @Query("SELECT * FROM to_do")
+    suspend fun getTasks(): List<CardInfo>
 }
-
-
-
-
-
-
-
--- app/src/main/java/com/example/todo_lab4/DataObject.kt
+++ app/src/main/java/com/example/todo_lab4/DataObject.kt
@@ -2,34 +2,39 @@ package com.example.todo_lab4
 
 import java.util.Date
 
-
+// Singleton object for managing task data
 object DataObject {
+    // List to store task data
     var listdata = mutableListOf<CardInfo>()
 
+    // Method to add new task data to the list
     fun setData(title: String, priority: String, date: Date) {
-        listdata.add(CardInfo(title, priority,date ))
+        listdata.add(CardInfo(title, priority, date))
     }
 
+    // Method to retrieve all task data from the list
     fun getAllData(): List<CardInfo> {
         return listdata
     }
 
-    fun deleteAll(){
+    // Method to delete all task data from the list
+    fun deleteAll() {
         listdata.clear()
     }
 
-    fun getData(pos:Int): CardInfo {
+    // Method to retrieve task data at a specific position in the list
+    fun getData(pos: Int): CardInfo {
         return listdata[pos]
     }
 
-    fun deleteData(pos:Int){
+    // Method to delete task data at a specific position in the list
+    fun deleteData(pos: Int) {
         listdata.removeAt(pos)
     }
 
-    fun updateData(pos:Int,title:String,priority:String)
-    {
-        listdata[pos].title=title
-        listdata[pos].priority=priority
+    // Method to update task data at a specific position in the list
+    fun updateData(pos: Int, title: String, priority: String) {
+        listdata[pos].title = title
+        listdata[pos].priority = priority
     }
-
 }
